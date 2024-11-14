61094001b1e02e7471b362273f0f8269789be478
shimodi <am.shahmoradi93@gmail.com>
2024-11-14 14:06:00 +0330

    base idea of product model implemented

---
+++ entities/Product.go
@@ -0,0 +1,9 @@
+package entities
+
+type Product struct {
+    ID          uint    `json:"id" gorm:"primaryKey"`
+    Name        string  `json:"name"`
+    Description string  `json:"description"`
+    Price       float64 `json:"price"`
+    Stock       int     `json:"stock"`
+}
