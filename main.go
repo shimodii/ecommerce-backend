237ef5ae36951d09d9a3e4b8c309f306069647e8
shimodi <am.shahmoradi93@gmail.com>
2024-11-14 14:54:50 +0330

    calling database init function and setting a simple route on root

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/shimodii/ecommerce-backend/repository"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	app := fiber.New()
+
+	//calling repository InitDatabase to initialize database connection
+	repository.InitDatabase()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("everything is up")
+	})
 }
