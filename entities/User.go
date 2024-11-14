da196f68827e6cbe6b3098f7456066da05beda7c
shimodi <am.shahmoradi93@gmail.com>
2024-11-14 14:01:19 +0330

    base idea of user model implemented

--- entities/User.go
+++ entities/User.go
@@ -1,5 +1,9 @@
 package entities
 
 type User struct {
-    ID      uint    `json:"id" gorm:"primaryKey"`
+    ID          uint    `json:"id" gorm:"primaryKey"`
+    Name        string  `json:"name"`
+    Email       string  `json:"email" gorm:"uique"`
+    Password    string  `json:"_"`
+    Role        string  `json:"role"`
 }
