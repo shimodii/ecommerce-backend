c68db69f3e03e23bf45866b067238dd0499b30ee
shimodi <am.shahmoradi93@gmail.com>
2024-12-22 15:02:41 +0330

    public and private routes for product model done

--- router/Router.go
+++ router/Router.go
@@ -1,19 +1,32 @@
 package router
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/shimodii/ecommerce-backend/controllers"
+	"github.com/shimodii/ecommerce-backend/middleware"
+)
 
 func SetupRouter(app *fiber.App){
     // api for public apis
     api := app.Use("api")
 
     // admin for admin apis
-    // admin := app.Use("admin")
+    admin := app.Use("admin", middleware.AdminMiddleware)
+
+    // admin routes
+    admin.Get("/", func(c *fiber.Ctx) error {
+        return c.SendString("welcome admin")
+    })
+    admin.Post("/new-product", controllers.AddProduct)
+    admin.Put("/update-product", controllers.UpdateProduct)
+    admin.Delete("/delete-product", controllers.DeleteProduct)
+
+    // public routes
+    api.Get("/all-products", controllers.GetAllProducts)
+    api.Get("/product/:id", controllers.GetProduct)
 
     //healthcheck
     app.Get("/status", func(c *fiber.Ctx) error {
         return c.SendString("Server is UP!!")
     })
-
-    //api.Post("/register", )
-
 }
