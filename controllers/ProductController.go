9ba7369466cc01746f48242c3e152c42e0647907
shimodi <am.shahmoradi93@gmail.com>
2024-12-22 14:14:17 +0330

    name changed

---
+++ controllers/ProductController.go
@@ -0,0 +1,13 @@
+package controllers
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/shimodii/ecommerce-backend/entities"
+	"github.com/shimodii/ecommerce-backend/repository"
+)
+
+func GetAllProducts(c *fiber.Ctx) error {
+    var products []entities.Product
+
+    
+}
